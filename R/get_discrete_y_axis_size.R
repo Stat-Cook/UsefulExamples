07b0e2a454db7af1e062ca302a9ddd8a787e257f
RM Cook <51404549+Stat-Cook@users.noreply.github.com>
2024-10-28 16:40:21 +0000

    Update get_discrete_y_axis_size.R (copilot)

--- R/get_discrete_y_axis_size.R
+++ R/get_discrete_y_axis_size.R
@@ -1,35 +1,29 @@
 # Detect the size of a discrete y-axis in a ggplot.
 # For use with ggsave to scale relative to elements being plotted.
 
-get_discrete_y_axis_size <- function(.plt){
+get_discrete_y_axis_size <- function(.plt) {
   #' Get the range of a discrete y-axis.
-  #' 
+  #'
   #' @param .plt A ggplot object.
-  #' 
+  #'
   #' @return The range of the y-axis.
-  #' 
+  #'
   #' @examples
-  #' 
-  #' plt <- ggplot(mtcars, aes(x=mpg, y=as.factor(cyl))) + geom_point()
-  #' 
+  #' plt <- ggplot(mtcars, aes(x = mpg, y = as.factor(cyl))) + geom_point()
   #' get_discrete_y_axis_size(plt)
-  #' 
+  
+  # Build the ggplot object
   .built <- ggplot_build(.plt)
   
-  .bool <- .built$data |>
-    map("y") |>
-    map_lgl(~"mapped_discrete" %in% class(.)) |>
-    simplify() |>
-    all()
+  # Check if y-axis is discrete
+  is_discrete <- all(map_lgl(.built$data, ~ "mapped_discrete" %in% class(.x$y)))
   
-  if (!.bool){
+  if (!is_discrete) {
     rlang::abort("Expecting 'discrete' y-axis mapping.")
   }
   
-  .built$data |>
-    map("y") |>
-    map_dbl(max) |>
-    simplify() |>
-    max()
+  # Get the maximum value of the y-axis
+  max_y <- max(map_dbl(.built$data, ~ max(.x$y, na.rm = TRUE)))
   
+  return(max_y)
 }
