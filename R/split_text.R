0d75bbf4db9399543dcbf0eed7349bfd4d03003a
RM Cook <51404549+Stat-Cook@users.noreply.github.com>
2024-10-28 14:07:26 +0000

    Create split_text.R

---
+++ R/split_text.R
@@ -0,0 +1,20 @@
+# function to split text over multiple lines
+
+split_text <- function(text, width = 30) {
+  #' This code will split the `text` into multiple lines.
+  #' You can adjust the `width` parameter to control 
+  #' the maximum width of each line.
+  #' 
+  #' @param text A character vector of text to split.
+  #' @param width An integer specifying the maximum width of each line.
+  #' 
+  #' @return A character vector of text split into multiple lines.
+  #' 
+  #' @examples 
+  #' split_text("This is a long string that needs to be split into multiple lines.", width = 20)
+  #' 
+  map(text, strwrap, width = width) |>
+    map(paste, collapse="\n") |>
+    unlist()
+
+}
